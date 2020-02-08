239115c570f25c9d031952e8269e77f9051ca948
Greg Tyndall <greg@bloinka.com>
2020-02-07 19:56:02 -0600

    .

--- src/templates/special-post.js
+++ src/templates/special-post.js
@@ -6,7 +6,7 @@ import { graphql, Link } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
 
-export const BlogPostTemplate = ({
+export const SpecialPostTemplate = ({
   content,
   contentComponent,
   description,
@@ -46,7 +46,7 @@ export const BlogPostTemplate = ({
   );
 };
 
-BlogPostTemplate.propTypes = {
+SpecialPostTemplate.propTypes = {
   content: PropTypes.node.isRequired,
   contentComponent: PropTypes.func,
   description: PropTypes.string,
@@ -54,12 +54,11 @@ BlogPostTemplate.propTypes = {
   helmet: PropTypes.object
 };
 
-const BlogPost = ({ data }) => {
+const SpecialPost = ({ data }) => {
   const { markdownRemark: post } = data;
-
   return (
     <Layout>
-      <BlogPostTemplate
+      <SpecialPostTemplate
         content={post.html}
         contentComponent={HTMLContent}
         description={post.frontmatter.description}
@@ -79,16 +78,16 @@ const BlogPost = ({ data }) => {
   );
 };
 
-BlogPost.propTypes = {
+SpecialPost.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.object
   })
 };
 
-export default BlogPost;
+export default SpecialPost;
 
 export const pageQuery = graphql`
-  query BlogPostByID($id: String!) {
+  query SpecialPostByID($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
       html
